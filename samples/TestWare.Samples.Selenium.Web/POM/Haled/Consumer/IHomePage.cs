f7f2b3c2b706bc92d17ae62800aa80e183714e3f
jesussalatiel <jesussalatiel>
2022-11-10 13:28:01 -0600

    Changes

--- samples/TestWare.Samples.Selenium.Web/POM/Haled/Consumer/IHomePage.cs
+++ samples/TestWare.Samples.Selenium.Web/POM/Haled/Consumer/IHomePage.cs
@@ -5,8 +5,12 @@ namespace TestWare.Samples.Selenium.Web.POM.Haled.Consumer;
 /// <summary>
 /// Encapsulate all Loging busines logic
 /// </summary>
-public interface ILoginPage
+public interface IHomePage
 {
-    
+    /// <summary>
+    /// Script to send User Name
+    /// </summary>
+    /// <param name="name"></param>
+    void EnterUserName(string name);
 }
 
